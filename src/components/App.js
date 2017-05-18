fa5195cc0503af55219c13fb572493fc2c065d3c
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-05-17 21:42:06 -0700

    update App component: render AddTodo and TodoList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import AddTodo from "../containers/AddTodo";
+import TodoList from "./TodoList";
 import '../css/App.css';
 
+
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
-
+        <AddTodo /> <br />
+        Show Todos: <br />
+        <TodoList />
       </div>
     );
   }
