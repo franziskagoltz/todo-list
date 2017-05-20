b8c9aa445d19dd9efe52589ce035aaea2cb883c0
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-05-19 18:45:57 -0700

    new container: filter visible todos

---
+++ src/containers/VisibleTodos.js
@@ -0,0 +1,14 @@
+import {connect} from 'react-redux';
+
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed)
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed)
+  }
+}
+
